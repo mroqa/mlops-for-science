11afcaabd413bfe2e6e6dedbbb5c79f9f08c3f3c
Mohammed Roqa <mroqa@outlook.com>
2023-01-02 15:23:16 +0100

    fixing project dependencies

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
+# import dash_core_components as dcc
+# import dash_html_components as html
 from dash.dependencies import Input, Output, ClientsideFunction
 
 import numpy as np
